9e1997e06492a20d0db1a1d65292ad12803cf579
fengzhongsen <fengzhongsen@gmail.com>
2018-04-30 12:27:08 +0800

    Add two controllers.

---
+++ server/controllers/record.js
@@ -0,0 +1,22 @@
+var create_record = async (ctx, next) => {
+
+};
+
+var delete_record = async (ctx, next) => {
+
+};
+
+var update_record = async (ctx, next) => {
+
+};
+
+var get_records = async (ctx, next) => {
+
+};
+
+module.exports = {
+  'POST /record': create_record,
+  'DELETE /record': delete_record,
+  'PUT /record': update_record,
+  'GET /record': get_records
+};
---
+++ server/controllers/template.js
@@ -0,0 +1,22 @@
+var create_template = async (ctx, next) => {
+
+};
+
+var delete_template = async (ctx, next) => {
+
+};
+
+var update_template = async (ctx, next) => {
+
+};
+
+var get_templates = async (ctx, next) => {
+
+};
+
+module.exports = {
+  'POST /template': create_template,
+  'DELETE /template': delete_template,
+  'PUT /template': update_template,
+  'GET /template': get_templates
+};
