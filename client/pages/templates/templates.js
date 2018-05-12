284e7c72729ac4038f9243b481be8da738dd6a53
fengzhongsen <fengzhongsen@gmail.com>
2018-05-12 14:16:19 +0800

    Add four pages.

---
+++ client/pages/templates/templates.js
@@ -0,0 +1,86 @@
+// pages/templates/templates.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    templates: []
+  },
+
+  getTemplates() {
+    util.showBusy('请求中...');
+    var that = this
+    var options = {
+      ...config.option('template'),
+      success(result) {
+        util.showSuccess('请求成功完成');
+        console.log('request success', result);
+        that.setData({
+          templates: JSON.stringify(result.data)
+        })
+      },
+      fail(error) {
+        util.showModel('请求失败', error);
+        console.log('request fail', error);
+      }
+    };
+    qcloud.request(options)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
