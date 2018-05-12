9fa5a98f64cbdd0f55a6af60be2f960b85c8c0de
fengzhongsen <fengzhongsen@gmail.com>
2018-05-12 13:49:35 +0800

    Modified nproxy from weapp to api.

--- server/routes/index.js
+++ server/routes/index.js
@@ -50,7 +50,7 @@ module.exports = function (dir) {
   let
     controllers_dir = dir || 'controllers',
     router = require('koa-router')({
-      prefix: '/weapp'
+      prefix: '/api'
     });
   addControllers(router, controllers_dir);
   return router.routes();
